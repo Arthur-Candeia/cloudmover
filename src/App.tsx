6adbfade133cb30772a1198ff8f430c18ce051f0
Arthur-Candeia <arthurheher@hotmail.com>
2023-07-16 22:32:23 -0300

    versao final 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -19,32 +19,43 @@ function App() {
     <Start />
     <main>
       <header>
-        <h1>CLOUDMOVE</h1>
+        <h1>CLOUDMOVER</h1>
         <span>NÓS SOMOS O FUTURO</span>
+        <nav>
+          <a href="#whoWeAre">Quem somos?</a>
+          <a href="#whatWeDo">O que fazemos?</a>
+          <a href="#map">Mapa interativo</a>
+          <a href="#investments">Investimentos</a>
+          <a href="#Responsibilities">Responsabilidades</a>
+          <a href="#technology">Tecnologia</a>
+          <a href="#contact">Contate-nos</a>
+        </nav>
       </header>
       <div id="content">
         <section id="banner">
           <img src="./banner/cloud1.jpg" alt="Cloudmover" id="bannerImg"/>
           <span>VAMOS <br />MELHORAR <br />O MUNDO</span>
         </section>
-        <h2>Quem somos?</h2>
+        <a id='backToTop' href='#content'>⬆️</a>
+        <h2 id='whoWeAre'>Quem somos?</h2>
         <p>
           A CloudMover é uma startup inovadora que está revolucionando a forma como interagimos com as nuvens. Fundada por um grupo de potenciais Cientistas da Computação da Universidade Federal do Espírito que são apaixonados pela preservação e sustentabilidade ambiental, nossa empresa tem como objetivo combater a escassez de chuva em áreas secas e melhorar a distribuição equitativa de água no ambiente. Acreditamos que a água é um recurso vital e essencial para a vida, e é por isso que desenvolvemos uma tecnologia pioneira para fabricar e movimentar nuvens.
         </p>
-        <h2>O que fazemos?</h2>
+        <h2 id='whatWeDo'>O que fazemos?</h2>
         <p>
           Na CloudMover, utilizamos uma abordagem inovadora que envolve a condensação de aerossóis para induzir a formação de nuvens em áreas secas. Por meio de extensas pesquisas e experimentos, desenvolvemos uma metodologia eficiente que consiste no lançamento de pequenas partículas, como sal marinho ou sulfato, na atmosfera. Essas partículas atuam como núcleos de condensação, permitindo que o vapor de água presente na atmosfera se agrupe em torno delas, formando nuvens. Com o uso de tecnologias avançadas, somos capazes de movimentar essas nuvens, direcionando-as para áreas específicas que sofrem com a escassez de chuva. Dessa forma, podemos trazer alívio e benefícios para regiões afetadas pela seca.
         </p>
-        <h2>Mapa interativo</h2>
+        <img src="./whatwedo.jpg" alt="Imagem de pessoa olhando para a chuva por uma janela" id='whatWeDoImg'/>
+        <h2 id='map'>Mapa interativo</h2>
         <p>
           Para proporcionar transparência e permitir que nossos clientes, principalmente entidades governamentais, acompanhem de perto nossas operações, desenvolvemos um mapa interativo em nosso site. Essa ferramenta inovadora permite visualizar em tempo real as áreas que possuem seca, onde as nuvens poderiam estar sendo criadas e as regiões que seriam beneficiadas com a chuva induzida.
         </p>
         <iframe title="Untitled" src="https://ion.cesium.com/stories/viewer/?id=88b3500c-619d-478d-a7e2-f9cf29d5363f" allow="fullscreen" allowFullScreen={true} ></iframe>
-        <h2>Investimentos</h2>
+        <h2 id='investments'>Investimentos</h2>
         <p>
           Estamos em busca de investimentos estratégicos para impulsionar o crescimento e aprimorar ainda mais nossa tecnologia. Reconhecemos que a pesquisa e o desenvolvimento contínuo são fundamentais para aperfeiçoar nossa abordagem de condensação de aerossóis e maximizar seus benefícios. Além disso, queremos expandir nossa capacidade de atender a uma maior demanda de áreas afetadas pela seca em todo o mundo. Se você está interessado em investir em nossa startup inovadora e fazer parte dessa jornada revolucionária, entre em contato conosco para discutir oportunidades de investimento. Estamos abertos a parcerias estratégicas e a colaborações que possam impulsionar nosso crescimento e impacto positivo no combate à escassez de água.
         </p>
-        <h2>Responsabilidade social/ambiental</h2>
+        <h2 id='Responsibilities'>Responsabilidade social/ambiental</h2>
         <p>
           Na CloudMover, assumimos uma responsabilidade sólida em relação aos aspectos sociais e ambientais de nossas operações. Estamos comprometidos em garantir que nosso trabalho esteja em conformidade com as normas ambientais e de segurança, além de minimizar quaisquer efeitos adversos associados à nossa tecnologia. Trabalhamos em estreita colaboração com especialistas ambientais e reguladores para garantir que nossas operações sejam conduzidas com os mais altos padrões éticos e sustentáveis. Além disso, promovemos ações de responsabilidade social, buscando parcerias com organizações locais e internacionais para desenvolver programas de conscientização e sustentabilidade. Nossos esforços visam promover o uso responsável e ético de nossa tecnologia, contribuindo para a conservação dos recursos hídricos e o bem-estar das comunidades afetadas pela seca.
         </p>
@@ -54,11 +65,11 @@ function App() {
           <Card symbol="🌳" name="Biodegradável"></Card>
           <Card symbol="☁️" name="Clima"></Card>
         </section>
-        <h2>Tecnologia</h2>
+        <h2 id='technology'>Tecnologia</h2>
         <p>
           A tecnologia desenvolvida pela CloudMover é baseada uma pesquisa e desenvolvimento intensivos na área de ciência atmosférica e tecnológica. Nossa equipe de especialistas altamente qualificados e dedicados trabalha constantemente para aprimorar nossa metodologia e otimizar a eficiência do processo de condensação de aerossóis. Investimos em tecnologia avançada e em parcerias estratégicas para impulsionar a inovação e aprimorar a viabilidade em larga escala de nossa técnica. Além disso, continuamos a realizar estudos e pesquisas para entender melhor os efeitos ambientais, os impactos climáticos e a interação com os ecossistemas locais. Nossa abordagem tecnológica está em constante evolução, sempre em busca de soluções mais eficientes e sustentáveis para enfrentar os desafios relacionados à escassez de chuva e à gestão de recursos hídricos.
         </p>
-        <h2>Contate-nos</h2>
+        <h2 id='contact'>Contate-nos</h2>
         <p>
           Se você deseja obter mais informações sobre a CloudMover, nossa tecnologia de criação de nuvens ou explorar possíveis parcerias e oportunidades de investimento, entre em contato conosco. Estamos ansiosos para compartilhar informações adicionais, responder a perguntas e discutir como podemos trabalhar juntos para enfrentar os desafios da escassez de água e promover um futuro mais sustentável e resiliente.
         </p>
--- src/start.tsx
+++ src/start.tsx
@@ -2,15 +2,17 @@ const body = document.querySelector('body')
 
 if (body) {
   body.style.backgroundImage = 'none'
+  body.style.backgroundColor = 'skyblue'
   setTimeout(() => {
     const main = document.querySelector('main')
     const cloud1 = document.getElementById('cloud1')
     const cloud2 = document.getElementById('cloud2')
     const textStart = document.getElementById('textStart')
     if (main && cloud1 && cloud2 && textStart) {
-      body.style.overflow = 'auto'
+      body.style.overflowY = 'auto'
       main.style.visibility = 'visible'
       body.style.backgroundImage = ''
+      body.style.backgroundColor = ''
       cloud1.style.display = 'none'
       cloud2.style.display = 'none'
       textStart.style.display = 'none'
